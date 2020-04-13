21ac49ce657af942842103625e54bcfa6980dc13
Kalix1984 <istvn.horvath@yahoo.com>
2020-04-13 22:43:15 +0200

    design

--- NukaTapperGame/src/application/Panel.java
+++ NukaTapperGame/src/application/Panel.java
@@ -6,40 +6,15 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
 public class Panel extends BorderPane {
-	private int width;
-	private int height;
-	private int posX;
-	private int posY;
-
-	Header title;
-	
-	Pane content;
-	StackPane footer;
 
 	public Panel(int width, int height, int posX, int posY) {
-		this.width = width;
-		this.height = height;
-		
-		this.posX = posX;
-		this.posY = posY;
 
-		this.setPrefWidth(width);
-		this.setPrefHeight(height);
+		setPrefWidth(width);
+		setPrefHeight(height);
 
-		this.setLayoutX(posX);
-		this.setLayoutY(posY);
+		setLayoutX(posX);
+		setLayoutY(posY);
 		
-		this.setTop(title);
-		this.setCenter(content);
-		this.setBottom(footer);
 		
 	}
-	
-	public void addHeader() {
-		Pane pane = new Pane();
-		pane.setStyle("-fx-background-color: white;");
-		this.getChildren().add(pane);
-	}
-
-	
 }
--- NukaTapperGame/src/application/menu/Toplist.java
+++ NukaTapperGame/src/application/menu/Toplist.java
@@ -3,12 +3,15 @@ package application.menu;
 import application.Panel;
 import application.Header;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Toplist {
@@ -17,34 +20,50 @@ public class Toplist {
 	public Toplist(Stage mainStage) {
 		this.rootPane = new AnchorPane();
 		
-//		MenuButton backButton = new MenuButton("Back");
-//		backButton.setOnAction(e -> {
-//			GameMenu mainMenu = new GameMenu(mainStage);
-//			
-//			mainStage.getScene().setRoot(mainMenu.getRootPane());
-//		});
+		initToplistView(mainStage);
+		
+	}
 
+	private void initToplistView(Stage mainStage) {
+		MenuButton backButton = new MenuButton("Vissza");
+		backButton.setAlignment(Pos.CENTER);
+		backButton.setOnAction(e -> {
+			GameMenu mainMenu = new GameMenu(mainStage);
+			
+			mainStage.getScene().setRoot(mainMenu.getRootPane());
+		});
 		
+		Panel toplistPanel = new Panel(250, 200, 260, 150);
 		
-		Panel toplistPanel = new Panel(400, 400, 200, 80);
-		toplistPanel.addHeader();
+		StackPane header = new StackPane();
+		header.setPadding(new Insets(5));
+		header.setStyle("-fx-background-color: #162455; -fx-background-radius: 10 10 0 0;");
 		
+		Text caption = new Text("Toplista");
+		caption.setFont(new Font("Impact", 20));
+		caption.setFill(Color.web("#AFD8E2"));
+		header.getChildren().add(caption);
 		
-//		titlePane.setStyle("-fx-background-color: black; -fx-background-radius: 10 10 0 0;");
-//		titlePane.setPadding(new Insets(5));
-//		toplistPanel.getTitlePane().setTop(titlePane);
-//		
-//		
-//		Label panelTitle = new Label();
-//		panelTitle.setFont(new Font(titleFontType, titleFontSize));
-//		panelTitle.setText(title);
-//		panelTitle.setTextFill(Color.web(titleFontColor));
-//
-//		titlePane.getChildren().add(panelTitle);
-
+		//tesztadatok
+		Text content1 = new Text("1. Jani");
+		Text content2 = new Text("2. Szilvi");
+		Text content3 = new Text("3. András");
+		Text content4 = new Text("4. Attila");
+		Text content5 = new Text("5. Barabás");
+		
+		StackPane footer = new StackPane();
+		footer.setPadding(new Insets(10));
+		footer.getChildren().add(backButton);
+		
+		VBox content = new VBox();
+		content.setPadding(new Insets(20, 50, 20, 50));
+		content.getChildren().addAll(content1, content2, content3, content4, content5);
+		
+		toplistPanel.setTop(header);
+		toplistPanel.setCenter(content);
+		toplistPanel.setBottom(footer);
 
 		rootPane.getChildren().add(toplistPanel);
-		
 	}
 
 	public Pane getRootPane() {

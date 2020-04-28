4600e160e46a881a4cec9e143f73e626d4d481ff
hu0hois <hu0hois@CHU89669.festo.net>
2020-04-28 14:00:56 +0200

    -

---
+++ NukaTapperGame/src/application/entities/Mob.java
@@ -0,0 +1,10 @@
+package application.entities;
+
+public interface Mob {
+	//update valószínűleg különbözni fog a játékos és a guest között
+	public void update();
+	//rendereléshez 2 változatot használok....téglalap és kép...de alapvetően minden entitinél ugyan az.
+//	public void render();
+	
+
+}
